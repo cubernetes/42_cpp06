feaed8fb82ff3d6cb0066d63bf7d697915ae08be
tsi <timo42@proton.me>
2025-04-17 03:46:00 +0200

    Convince evalutators

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -20,6 +20,7 @@ int main() {
     std::cout << "Serialized data: " << Serializer::serialize(&data1) << std::endl;
     std::cout << "Deserialized data: ";
     print_data(Serializer::deserialize(Serializer::serialize(&data1)));
+    std::cout << "Deserialize(Serialize(data))==data: " << (&data1 == Serializer::deserialize(Serializer::serialize(&data1)) ? "yes" : "no") << std::endl;
 
     std::cout << "\nTesting data2" << std::endl;
     std::cout << "Data: ";
@@ -27,6 +28,7 @@ int main() {
     std::cout << "Serialized data: " << Serializer::serialize(&data2) << std::endl;
     std::cout << "Deserialized data: ";
     print_data(Serializer::deserialize(Serializer::serialize(&data2)));
+    std::cout << "Deserialize(Serialize(data))==data: " << (&data2 == Serializer::deserialize(Serializer::serialize(&data2)) ? "yes" : "no") << std::endl;
 
     std::cout << "\nTesting data3" << std::endl;
     std::cout << "Data: ";
@@ -34,5 +36,7 @@ int main() {
     std::cout << "Serialized data: " << Serializer::serialize(&data3) << std::endl;
     std::cout << "Deserialized data: ";
     print_data(Serializer::deserialize(Serializer::serialize(&data3)));
+    std::cout << "Deserialize(Serialize(data))==data: " << (&data3 == Serializer::deserialize(Serializer::serialize(&data3)) ? "yes" : "no") << std::endl;
+
     return 0;
 }
